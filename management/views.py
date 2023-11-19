cbc4952b71fe4483a6aa03e7bea4e8d42f1e6e4c
R Ashwin <ashwin3082002@gmail.com>
2023-11-19 22:55:51 +0530

    django messages setup

--- management/views.py
+++ management/views.py
@@ -3,4 +3,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def index(request):
+    messages.info(request, "Hello world!")
+    
     return render(request, 'index.html')
